7edafc28c73c486030e01ec01a24d0330b8b000b
ntlee1 <53658810+ntlee1@users.noreply.github.com>
2021-10-13 16:25:30 -0500

    Refactor

--- scripts/01_init.R
+++ scripts/01_init.R
@@ -9,120 +9,89 @@ lapply(rs$pkg_req, require, character.only = TRUE)
 rs$rs_fpath <- here::here("data", "RiskSpanSkillsAssessment.xlsx")
 rs$rs_data <- readxl::read_xlsx(path = rs$rs_fpath, "Data")
 
+#Ensure all loan number ids are unique #TRUE
+length(unique(rs$rs_data$LOAN_NUMBER)) == length(rs$rs_data$LOAN_NUMBER)
+
+#Custom Categorical Cols
+rs$rs_data$FICO_CAT <- cut(rs$rs_data$FICO_SCORE, breaks = c(0, 600, 700, 800, 850), 
+                           include.lowest = TRUE, right = FALSE, 
+                           labels = c("0-600", "600-700", "700-800", "800-850"))
+rs$rs_data$LTV_CAT <- cut(rs$rs_data$LTV, breaks = c(0, 85, 90, 95, 100),
+                          include.lowest = TRUE, right = TRUE,
+                          labels = )
+
+rs$rs_data$LOAN_AGE <- round((rs$rs_data$START_DATE - rs$rs_data$LOAN_ORIG_DATE)/30, 2) %>%
+  as.numeric()
+rs$rs_data$LOAN_AGE_CAT <- cut((rs$rs_data$LOAN_AGE), breaks = c(0, 10, 20, 30, 40, 70),
+                               include.lowest = TRUE, right = FALSE)
+
 #Report 1
 rs$rs_data$LENDER_INST_TYPE_DESCRIPTION <- as.factor(rs$rs_data$LENDER_INST_TYPE_DESCRIPTION)
 rs$lend_levels <- levels(rs$rs_data$LENDER_INST_TYPE_DESCRIPTION)
 
-rp1_fun <- function(levels) {
-  my_levels <- levels
-  level_data <- dplyr::filter(rs$rs_data, LENDER_INST_TYPE_DESCRIPTION == rs$lend_levels[my_levels])
-  
-  loan_count <- nrow(level_data)
-  avg_curr <- mean(level_data$CURRENT_BALANCE)%>%
-    round(digits = 2)
-  max_curr <- max(level_data$CURRENT_BALANCE) %>%
-    round(digits = 2)
-  min_curr <- min(level_data$CURRENT_BALANCE) %>%
-    round(digits = 2)
-  
-  my_results <- data.frame(Institution_Name = rs$lend_levels[my_levels],
-                           Loan_Count = loan_count,
-                           Avg_Current_Balance = avg_curr,
-                           Max_Current = max_curr,
-                           Min_Current = min_curr)
-  return(my_results)
-}
-
-rs$rp1 <- lapply(1:5, rp1_fun)
-rs$rp1 <- as.data.frame(do.call(rbind, rs$rp1))
-print(rs$rp1)
-
+rs$rs_data %>%
+dplyr::group_by(LENDER_INST_TYPE_DESCRIPTION) %>%
+  summarise("Loan Count" = n(),
+            "Avg Current Balance" = mean(CURRENT_BALANCE),
+            "Max Current Balance" = max(CURRENT_BALANCE),
+            "Min Current Balance" = min(CURRENT_BALANCE))
 #Report 2
-rs$rs2_breaks <- c("<=85%",
-                   ">85% & <=90%",
-                   ">90% & <=95%",
-                   ">95%")
-break_1 <- filter(rs$rs_data, LTV <= 85) 
-break_2 <- filter(rs$rs_data, 85 < LTV & LTV <= 90) 
-break_3 <- filter(rs$rs_data, 90 < LTV & LTV <= 95)   
-break_4 <- filter(rs$rs_data, 95 < LTV)
-
-rp2_fun <- function(breaks) {
-  my_break <- breaks
-  
-  loan_count <- nrow(breaks)
-  avg_curr <- mean(breaks$CURRENT_BALANCE)%>%
-    round(digits = 2)
-  max_curr <- max(breaks$CURRENT_BALANCE) %>%
-    round(digits = 2)
-  min_curr <- min(breaks$CURRENT_BALANCE) %>%
-    round(digits = 2)
-  
-  my_results <- data.frame(Loan_Count = loan_count,
-                           Avg_Current_Balance = avg_curr,
-                           Max_Current = max_curr,
-                           Min_Current = min_curr)
-  return(my_results)
-}
-
-rs$rs2_results <- rbind(rp2_fun(break_1),
-                        rp2_fun(break_2),
-                        rp2_fun(break_3),
-                        rp2_fun(break_4))
-rs$rs2_results <- cbind("Breaks" = rs$rs2_breaks, rs$rs2_results)
-print(rs$rs2_results)
+rs$rs_data %>%
+  dplyr::group_by(LTV_CAT) %>%
+  summarise("Loan Count" = n(),
+            "Avg Current Balance" = mean(CURRENT_BALANCE),
+            "Max Current Balance" = max(CURRENT_BALANCE),
+            "Min Current Balance" = min(CURRENT_BALANCE))
 
 #Report 3
-#Assume a month is 30 days
-rs$loan_ages <- c("Unknown",
-                  "0-9mth",
-                  "10-19mth",
-                  "20-29mth",
-                  "30-39mth",
-                  ">=40mth")
-rs$rs_data$LOAN_AGE <- (rs$rs_data$START_DATE - rs$rs_data$LOAN_ORIG_DATE)/30
+rs$rs_data %>%
+  dplyr::group_by(LOAN_AGE_CAT) %>%
+  summarise("Loan Count" = n(),
+            "Avg Current Balance" = mean(CURRENT_BALANCE),
+            "Max Current Balance" = max(CURRENT_BALANCE),
+            "Min Current Balance" = min(CURRENT_BALANCE))
 
-rs$age_na <- dplyr::filter(rs$rs_data, is.na(rs$rs_data$LOAN_AGE))
-rs$age_naomit <- rs$rs_data %>%
-  filter(!is.na(LOAN_AGE))
 
-rs$age_naomit_b1 <- dplyr::filter(rs$age_naomit, LOAN_AGE <= 10)
-rs$age_naomit_b2 <- dplyr::filter(rs$age_naomit, 10 < LOAN_AGE & LOAN_AGE <= 20)
-rs$age_naomit_b3 <- dplyr::filter(rs$age_naomit, 20 < LOAN_AGE & LOAN_AGE <= 30)
-rs$age_naomit_b4 <- dplyr::filter(rs$age_naomit, 30 < LOAN_AGE & LOAN_AGE <= 40)
-rs$age_naomit_b5 <- dplyr::filter(rs$age_naomit, 40 < LOAN_AGE)
+#Report 4
+rs$rs_data$CURRENT_UPB <- rs$rs_data$ORIG_VALUE - rs$rs_data$CURRENT_BALANCE
 
-rp3_fun <- function(data) {
-  mydata <- data
+rs$rs_data %>%
+  dplyr::group_by(FICO_CAT, LTV_CAT)%>%
+  summarise("Sum of Current UPB" = sum(na.omit(CURRENT_UPB)))
 
-  loan_count <- nrow(mydata)
-  avg_curr <- mean(mydata$CURRENT_BALANCE)%>%
-    round(digits = 2)
-  max_curr <- max(mydata$CURRENT_BALANCE) %>%
-    round(digits = 2)
-  min_curr <- min(mydata$CURRENT_BALANCE) %>%
-    round(digits = 2)
-  
-  my_results <- data.frame(Loan_Count = loan_count,
-                           Avg_Current_Balance = avg_curr,
-                           Max_Current = max_curr,
-                           Min_Current = min_curr)
-  return(my_results)
-}
 
-rs$rp3_results <- rbind(rp3_fun(rs$age_na),
-                         rp3_fun(rs$age_naomit_b1),
-                         rp3_fun(rs$age_naomit_b2),
-                         rp3_fun(rs$age_naomit_b3),
-                         rp3_fun(rs$age_naomit_b4),
-                         rp3_fun(rs$age_naomit_b5))
 
-rs$rp3_results <- cbind("Loan Age Cohorts" = rs$loan_ages, rs$rp3_results)
-print(rs$rp3_results)
 
-#Report 4 - Current Unpaid Balance of Loan (UPB)
+    
 
-      
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
       
       
       
